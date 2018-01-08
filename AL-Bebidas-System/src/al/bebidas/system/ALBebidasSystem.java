8904ca633e3c8074e1ea8e98b920c52d9716625d
Renan <renansccp077_@hotmail.com>
2018-01-08 17:14:07 -0300

    Commit de teste

---
+++ AL-Bebidas-System/src/al/bebidas/system/ALBebidasSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package al.bebidas.system;
+
+/**
+ *
+ * @author Renan
+ */
+public class ALBebidasSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo. Teste Netbeans");
+    }
+    
+}
